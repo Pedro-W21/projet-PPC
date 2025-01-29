62bc011d4121ee4f86966616981079415fe3b6d7
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-29 08:59:02 +0100

    création des fichiers utiles

---
+++ code/main.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process, Lock, Array, Value
+import sysv_ipc
+
+MQ_KEYS = [128, 256, 512, 1024]
+
+if __name__ == "__main__":
+    for key in MQ_KEYS:        
+        mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+
+    compteur_limiteur_normal = Value('i', 0, lock=False)
+    lock_limiteur_normal = Lock()
+
+    compteur_limiteur_prio = Value('i', 0, lock=False)
+    lock_limiteur_prio = Lock()
+
+    traffic_lights = Array('i', 4, lock=False)
+    traffic_lights_lock = Lock()
+
+
+
+
+
+    for key in MQ_KEYS:
+        mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+        mq.remove()
