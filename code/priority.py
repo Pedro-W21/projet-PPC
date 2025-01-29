fd4df416b9f01b87c9b135566f329dad0684109c
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-29 09:20:54 +0100

    lancement des process dans main

--- code/priority.py
+++ code/priority.py
@@ -1,2 +1,22 @@
+import random
+from multiprocessing import Process
 
 
+class Priority(Process):
+
+    def __init__(self, compteur_normal, keyQueues, lock):
+        super().__init__()
+        self.compteur_normal = compteur_normal
+        self.keyQueues = keyQueues
+        self.lock = lock
+
+    def normal_traffic_gen(self):
+        #choisit de manière random où mettre une voiture
+        depart = random.randint(0,3)
+        arrivee = random.randint(0,3)
+        #pour éviter que le chemin du départ soit l'arrivée
+        while arrivee == depart :
+            arrivee = random.randint(0,3)
+        texte = f"{self.keyQueues[depart]}_{self.keyQueues[arrivee]}"
+        mq.send(texte, type=1)
+            
