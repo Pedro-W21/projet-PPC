f17f0c97ea26c2a1a7929fa82cb054bfae5d06e4
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-01-31 08:24:51 +0100

    coordinator et sender implémentation de base

--- code/coordinator.py
+++ code/coordinator.py
@@ -1,10 +1,26 @@
 import random
-from multiprocessing import Process
+from multiprocessing import Process, Queue
+import sysv_ipc
+import signal
+import os
+import time
 
+PORT_COORD = 2000
+
+class Car:
+    def __init__(self, car_type, id, start, end):
+        self.car_type = car_type
+        self.id = id
+        self.start = start
+        self.end = end
+
+def car_from_string(car_message, car_type) -> Car:
+    split_message = car_message.split("_")
+    Car(car_type, int(split_message[0]), int(split_message[1]), int(split_message[2]))
 
 class Coordinator(Process):
 
-    def __init__(self, compteur_normal, lock_normal, compteur_prio, lock_prio, traffic_lights, traffic_lights_lock, keyQueues):
+    def __init__(self, compteur_normal, lock_normal, compteur_prio, lock_prio, traffic_lights, traffic_lights_lock, keyQueues, traffic_lights_pid, sent_message_queue, chemin_priorite_lock, mqpriorite):
         super().__init__()
         self.compteur_normal = compteur_normal
         self.lock_normal = lock_normal
@@ -13,3 +29,73 @@ class Coordinator(Process):
         self.traffic_lights = traffic_lights
         self.traffic_lights_lock = traffic_lights_lock
         self.keyQueues = keyQueues
+        self.message_queues = [sysv_ipc.MessageQueue(key) for key in keyQueues]
+        self.hanging_normal_cars = [None for i in range(4)]
+        self.hanging_prio_cars = [None for i in range(4)]
+        self.traffic_lights_pid = traffic_lights_pid
+        self.sent_messages_queue = sent_message_queue
+        self.chemin_prio_lock = chemin_priorite_lock
+        self.mq_priorite = mqpriorite
+
+
+    def send_car_passed(self, car:Car):
+        self.sent_messages_queue.put(f"car {car.id} {car.start} {car.end} {car.car_type} PASSED")
+
+    def run(self):
+        while True:
+            oldest_prio = None
+            for i in range(4):
+                if self.hanging_prio_cars[i] == None:
+                    try:
+                        car_message, ty = self.message_queues[i].receive(type=2, block=False)
+                        self.hanging_prio_cars[i] = car_from_string(car_message, ty)
+                        if oldest_prio == None:
+                            oldest_prio = i
+                        elif self.hanging_prio_cars[i].id < self.hanging_prio_cars[oldest_prio] and self.hanging_prio_cars[i-1] == None:
+                            oldest_prio = i
+                         
+                    except sysv_ipc.BusyError:
+                        pass
+                elif oldest_prio == None:
+                    oldest_prio = i
+                elif self.hanging_prio_cars[i].id < self.hanging_prio_cars[oldest_prio] and self.hanging_prio_cars[i-1] == None:
+                    oldest_prio = i
+                
+            if oldest_prio != None:
+                with self.lock_prio:
+                    self.compteur_prio.value -= 1
+                with self.chemin_prio_lock:
+                    self.mq_priorite.value = oldest_prio.start
+                # send signal to Lights
+                os.kill(self.traffic_lights_pid, signal.SIGKILL)
+                while True:
+                    with self.traffic_lights_lock:
+                        if self.traffic_lights[oldest_prio]:
+                            break
+                    time.sleep(0.01)
+                self.send_car_passed(self.hanging_normal_cars[oldest_prio])
+                self.hanging_normal_cars[oldest_prio] = None
+            else:
+                with self.traffic_lights_lock:
+                    oldest_passing_normal = None
+                    for i in range(4):
+                        if self.hanging_normal_cars[i] == None:
+                            try:
+                                car_message, ty = self.message_queues[i].receive(type=1, block=False)
+                                self.hanging_normal_cars[i] = car_from_string(car_message, ty)
+                                if oldest_passing_normal == None and self.traffic_lights[i]:
+                                    oldest_passing_normal = i
+                                elif self.hanging_normal_cars[i].id < self.hanging_normal_cars[oldest_passing_normal] and self.traffic_lights[i]:
+                                    oldest_passing_normal = i
+                            except sysv_ipc.BusyError:
+                                pass
+                        elif oldest_passing_normal == None and self.traffic_lights[i]:
+                            oldest_passing_normal = i
+                        elif self.hanging_normal_cars[i].id < self.hanging_normal_cars[oldest_passing_normal] and self.traffic_lights[i]:
+                            oldest_passing_normal = i
+                    if oldest_passing_normal != None:
+                        with self.lock_normal:
+                            self.compteur_normal.value -= 1
+                        self.send_car_passed(self.hanging_normal_cars[i])
+                        self.hanging_normal_cars[i] = None
+
---
+++ code/sender.py
@@ -0,0 +1,48 @@
+import time
+from queue import Empty
+from multiprocessing import Process, Queue
+import socket
+
+PORT_SENDER = 2025
+HOST_SENDER = "localhost"
+
+class Sender(Process):
+    def __init__(self, sent_messages_queue:Queue):
+        super(Sender, self).__init__()
+        self.sent_messages_queue = sent_messages_queue
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((HOST_SENDER, PORT_SENDER))
+        self.server_socket.listen(10)
+        self.clients = []
+        self.server_socket.setblocking(False)  # Set server socket to non-blocking
+
+    def run(self):
+        while True:
+            # Check if there's a message to send
+            try:
+                message = self.sent_messages_queue.get_nowait()
+                # Iterate through a copy in case the list changes during iteration
+                for client in list(self.clients):
+                    try:
+                        client.send(message.encode('utf-8'))
+                    except BrokenPipeError:
+                        # Client disconnected; remove from the list
+                        self.clients.remove(client)
+            except Empty:
+                # No messages to send; proceed to check for new connections
+                pass
+
+            # Attempt to accept new connections in a non-blocking way
+            try:
+                client_socket, addr = self.server_socket.accept()
+                client_socket.setblocking(False)
+                self.clients.append(client_socket)
+            except BlockingIOError:
+                # No pending connections; continue the loop
+                pass
+            except Exception as e:
+                # Handle any other exceptions that may occur
+                print(f"Exception occurred: {e}")
+
+            # Sleep briefly to prevent high CPU usage
+            time.sleep(0.1)
